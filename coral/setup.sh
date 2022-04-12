3e2b4c14a343fff0559d07d0d267d70f3e9f9c63
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-12 13:52:27 -0400

    start coral

--- coral/setup.sh
+++ coral/setup.sh
@@ -1,1 +1,3 @@
 sudo apt-get install libedgetpu1-std python3-pycoral
+sudo pip3 install picamera
+
---
+++ coral/test.sh
@@ -0,0 +1,3 @@
+python3 movenet_pose_estimation.py -m models/movenet_single_pose_lightning_ptq_edgetpu.tflite -i $1
+
+
