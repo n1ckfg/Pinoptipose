e8dcdeceba2cbe91ec42e24af0c9292f7e173072
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-12 21:40:49 -0400

    shell

--- coral/setup.sh
+++ coral/setup.sh
@@ -1,3 +1,9 @@
+# https://coral.ai/docs/accelerator/get-started/#runtime-on-linux
+
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+
 sudo apt-get install libedgetpu1-std python3-pycoral
 sudo pip3 install picamera
 
